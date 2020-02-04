bd6bf5c3aa1378b3b012e8f9a5e1e84461b6967a
Evgeny Kireev <mzkey.mail@gmail.com>
2020-02-05 03:28:35 +0900

    add operators

--- app/src/main/java/com/mzkey/complexconversion/Complex.kt
+++ app/src/main/java/com/mzkey/complexconversion/Complex.kt
@@ -3,46 +3,29 @@ package com.mzkey.complexconversion
 import kotlin.math.*
 
 class Complex {
-	private var a: Double = 0.0
-	private var b: Double = 0.0
-	private var arg: Double = 0.0
-	private var phi: Double = 0.0
+	var a: Double = 0.0
+		set(a: Double) {
+			field = a
+			convertFromAlgToExp()
+		}
 
-	fun setA(a: Double) {
-		this.a = a
-		convertFromAlgToExp()
-	}
+	var b: Double = 0.0
+		set(b: Double) {
+			field = b
+			convertFromAlgToExp()
+		}
 
-	fun setB(b: Double) {
-		this.b = b
-		convertFromAlgToExp()
-	}
+	var arg: Double = 0.0
+		set(arg: Double) {
+			field = arg
+			convertFromExpToAlg()
+		}
 
-	fun setArg(arg: Double) {
-		this.arg = arg
-		convertFromExpToAlg()
-	}
-
-	fun setPhi(phi: Double) {
-		this.phi = phi
-		convertFromExpToAlg()
-	}
-
-	fun getA(): Double {
-		return a
-	}
-
-	fun getB(): Double {
-		return b
-	}
-
-	fun getArg(): Double {
-		return arg
-	}
-
-	fun getPhi(): Double {
-		return phi
-	}
+	var phi: Double = 0.0
+		set(phi: Double) {
+			field = phi
+			convertFromExpToAlg()
+		}
 
 	private fun convertFromExpToAlg() {
 		this.a = (arg * cos(phi * (PI / 180)))
@@ -57,5 +40,34 @@ class Complex {
 		else if (this.a < 0 && this.b < 0) this.phi = atan(abs(this.b / this.a)) - 180.0
 		else if (this.a == 0.0 && this.b > 0) this.phi = 90.0
 		else if (this.a == 0.0 && this.b < 0) this.phi = -90.0
+	}
+
+	operator fun plus(other: Complex): Complex {
+		val rv: Complex = Complex()
+		rv.a = this.a + other.a
+		rv.b = this.b + other.b
+		return rv
+	}
+
+	operator fun minus(other: Complex): Complex {
+		val rv: Complex = Complex()
+		rv.a = this.a - other.a
+		rv.b = this.b - other.b
+		return rv
+	}
+
+	operator fun div(other: Complex): Complex {
+		val rv: Complex = Complex()
+		rv.arg = this.arg / other.arg
+		rv.phi = this.phi - other.phi
+		return rv
+	}
+
+	// times - mul
+	operator fun times(other: Complex): Complex {
+		val rv: Complex = Complex()
+		rv.arg = this.arg * other.arg
+		rv.phi = this.phi + other.phi
+		return rv
 	}
 }
