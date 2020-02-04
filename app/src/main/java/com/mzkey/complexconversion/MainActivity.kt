bbbe82efeabfcdca390b257067c1a925d5229052
Evgeny Kireev <mzkey.mail@gmail.com>
2020-02-05 04:16:54 +0900

    ui v0.2

--- app/src/main/java/com/mzkey/complexconversion/MainActivity.kt
+++ app/src/main/java/com/mzkey/complexconversion/MainActivity.kt
@@ -3,38 +3,44 @@ package com.mzkey.complexconversion
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.math.*
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+		var complexNumber = Complex()
+		var res: String
+		var signsAfterDot = 3
+		var atext: String
+		var btext: String
 
-        var atext: String
-        var btext: String
-        var a: Double
-        var b: Double
-        var arg: Double
-        var phi: Double
-        var res: String
-        var signsAfterDot: Int = 3
+		buttonCalculateAlg.setOnClickListener() {
+			if (editTextArg.text.isNotEmpty() && editTextPhi.text.isNotEmpty()) {
+				complexNumber.arg = editTextArg.text.toString().toDouble()
+				complexNumber.phi = editTextPhi.text.toString().toDouble()
+				atext = String.format("%.${signsAfterDot}f", complexNumber.a)
+				btext = String.format("%.${signsAfterDot}f", complexNumber.b)
+				res = if (complexNumber.b >= 0)
+					atext + "+" + btext + "i"
+				else
+					atext + btext + "i"
+				textViewAlgRes.text = res
+			} else textViewAlgRes.text = "Fill the fields"
+		}
 
-        buttonCalculate.setOnClickListener()
-        {
-            if (editTextArg.text.isNotEmpty() && editTextPhi.text.isNotEmpty()) {
-                arg = editTextArg.text.toString().toDouble()
-                phi = editTextPhi.text.toString().toDouble()
-                a = (arg * cos(phi * (PI / 180)))
-                b = (arg * sin(phi * (PI / 180)))
-                atext = String.format("%.${signsAfterDot}f", a)
-                btext = String.format("%.${signsAfterDot}f", b)
-                res = if (b >= 0)
-                    atext + "+" + btext + "i"
-                else
-                    atext + btext + "i"
-                textViewRes.text = res
-            } else textViewRes.text = "Fill the fields"
-        }
-    }
+		buttonCalculateExp.setOnClickListener() {
+			if (editTextA.text.isNotEmpty() && editTextB.text.isNotEmpty()) {
+				complexNumber.a = editTextA.text.toString().toDouble()
+				complexNumber.b = editTextB.text.toString().toDouble()
+				textViewResArg.text =
+					"arg: +${String.format("%.${signsAfterDot}f", complexNumber.arg)}"
+				textViewResPhi.text =
+					"phi: +${String.format("%.${signsAfterDot}f", complexNumber.arg)}"
+			} else {
+				textViewResArg.text = "Fill the fields"
+				textViewResPhi.text = ""
+			}
+		}
+	}
 }
